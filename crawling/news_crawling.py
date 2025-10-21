a235b1796ca342f74b952147252ceb27ec562131
hanlyang0522 <500nyanpark@gmail.com>
2025-10-22 01:29:22 +0900

    feat: 뉴스 크롤링 모듈 구조화 및 데이터베이스 연동 기능 구현

---
+++ crawling/news_crawling.py
@@ -0,0 +1,38 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+from db.db_news import get_connection, create_new_news, get_all_news
+
+
+header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+}
+
+# search = input("Enter search term: ")
+search = "당근마켓"
+url = "https://search.naver.com/search.naver?"
+
+params = {
+    "where": "news",
+    "query": search,
+    "start": 0,
+    "nso": "so:r,p:7d,a:all",  # 최근 7일
+}
+
+
+if __name__ == "__main__":
+    re_url = requests.get(url=url, headers=header, params=params)
+    html = BeautifulSoup(re_url.text, "html.parser")
+
+    articles = html.select('a[data-heatmap-target=".nav"]')
+
+    # Store news in the database
+    get_connection()
+
+    for article in articles:
+        title = article.text.strip()
+        url = article["href"].strip()
+        create_new_news(title, url)
+        print(f"Title: {title}\nURL: {url}\n")
+
+    print(f"Total articles fetched: {len(articles)}")
